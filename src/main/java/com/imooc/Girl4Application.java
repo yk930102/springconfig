3c5e0954169805085e25cad44c714621a65a1d88
yk930102 <aa303088@163.com>
2017-12-12 17:07:06 +0800

    project

---
+++ src/main/java/com/imooc/Girl4Application.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Girl4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Girl4Application.class, args);
+	}
+}
